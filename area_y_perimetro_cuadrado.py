f1a2da94674914d4586c5d527d5075da9cc6cc09
jmhernandez97 <167220360+jmhernandez97@users.noreply.github.com>
2024-04-16 16:47:53 +0200

    Create area_y_perimetro_cuadrado.py

---
+++ area_y_perimetro_cuadrado.py
@@ -0,0 +1,10 @@
+# 1 - Solicitar la longitud del lado
+lado = float(input("Por favor, introduce la longitud de un lado del cuadrado en cm: "))
+
+# 2 - Calcular el área y el perímetro
+area = lado * 2
+perimetro = lado * 4
+
+# 3 - Mostrar el resultado
+print(f"El resultado del área del cuadrado es: {area}")
+print(f"El resultado del perímetro del cuadrado es: {perimetro}")
